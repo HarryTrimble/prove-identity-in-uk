f7c304a1374c2f1667b2ab02d4a26a4fb35dfc7f
Harry <harryroberttrimble@gmail.com>
2017-05-14 15:15:59 +0100

    start adding Dutch journey

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,24 @@
-var express = require('express')
-var router = express.Router()
+	var express = require('express')
+	var fs = require('fs')
+	var marked = require('marked')
+	var path = require('path')
+	var router = express.Router()
+	var utils = require('../lib/utils.js')
 
-// Route index page
-router.get('/', function (req, res) {
-  res.render('index')
-})
+	// Branching
 
-// add your routes here
+	router.get('/single-brand/choose-method/time-in-uk', function (req, res) {
+	  // get the answer from the query string (eg. ?over18=false)
+	  var multiple_nationalities = req.session.data['multiple_nationalities']
 
-module.exports = router
+	  if (multiple_nationalities === 'Yes' && other_nationality === undefined) {
+	    // redirect to the relevant page
+	    res.redirect('/single-brand/choose-method/other-nationality')
+	  } else {
+	    // if over18 is any other value (or is missing) render the page requested
+	    res.render('single-brand/choose-method/time-in-uk')
+	  }
+	})
+
+	module.exports = router
+
