0c9a522c2e39525892b8f9aeab0fe1d4d126de84
Harry <harryroberttrimble@gmail.com>
2017-05-15 18:18:11 +0100

    finish single brand journey

--- app/routes.js
+++ app/routes.js
@@ -5,18 +5,74 @@
 	var router = express.Router()
 	var utils = require('../lib/utils.js')
 
+	// Route index page
+	router.get('/', function (req, res) {
+	  res.render('index')
+	})
+
 	// Branching
 
 	router.get('/single-brand/choose-method/time-in-uk', function (req, res) {
 	  // get the answer from the query string (eg. ?over18=false)
 	  var multiple_nationalities = req.session.data['multiple_nationalities']
+	  var other_nationality = req.session.data['other_nationality']
 
-	  if (multiple_nationalities === 'Yes' && other_nationality === undefined) {
-	    // redirect to the relevant page
+	  if (multiple_nationalities === 'Yes' && other_nationality === undefined ) {
+	    // if user DOES mutiple nationalities
 	    res.redirect('/single-brand/choose-method/other-nationality')
 	  } else {
-	    // if over18 is any other value (or is missing) render the page requested
+	    // if user ONE nationality
 	    res.render('single-brand/choose-method/time-in-uk')
+	  }
+	})
+
+	// Dutch journey
+
+	// /single-brand/eu/dutch/done-it-before
+
+	router.get('/single-brand/eu/dutch/registered', function (req, res) {
+	  // get the answer from the query string (eg. ?over18=false)
+	  var done_it_before = req.session.data['done_it_before']
+
+	  if (done_it_before === 'No' ) {
+	    // if user DOES mutiple nationalities
+	    res.redirect('/single-brand/eu/dutch/not-registered')
+	  } else {
+	    // if user ONE nationality
+	    res.render('single-brand/eu/dutch/registered/index.html')
+	  }
+	})
+
+	// /single-brand/eu/dutch/done-it-before
+
+	router.get('/single-brand/eu/dutch/registered', function (req, res) {
+	  // get the answer from the query string (eg. ?over18=false)
+	  var done_it_before = req.session.data['done_it_before']
+
+	  if (done_it_before === 'No' ) {
+	    // if user DOES mutiple nationalities
+	    res.redirect('/single-brand/eu/dutch/not-registered')
+	  } else {
+	    // if user ONE nationality
+	    res.render('single-brand/eu/dutch/registered/index.html')
+	  }
+	})
+
+	// /single-brand/eu/dutch/registered
+
+	router.get('/single-brand/eu/dutch/registered/password', function (req, res) {
+	  // get the answer from the query string (eg. ?over18=false)
+	  var login_type = req.session.data['login_type']
+
+	  if (login_type === 'text message' ) {
+	    // if user wants to login with TEXT MESSAGE
+	    res.redirect('/single-brand/eu/dutch/registered/text-message')
+	  } else if (login_type === "mobile app" ){
+	    // if user wants to login with MOBILE APP
+	    res.redirect('/single-brand/eu/dutch/registered/mobile-app')
+	  } else {
+	    // if user wants to login with USERNAME and PASSWORD
+	    res.render('single-brand/eu/dutch/registered/password/index.html')
 	  }
 	})
 
